13c603bfba906e728322ad3720e74f794e33aa89
anirudh <anirudhcseven@gmail.com>
2026-03-22 01:46:57 +0400

    Update OpenGraph font to Clash Display

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -6,6 +6,10 @@ export const size = { width: 1200, height: 630 };
 export const contentType = 'image/png';
 
 export default async function Image() {
+  const clashDisplayData = await fetch(
+    new URL('https://cdn.fontshare.com/wf/BFBSY7LX5W2U2EROCLVVTQP4VS7S4PC3/IIUX4FGTMD2LK2VWD3RVTAS4SSMUN7B5/53RZKGODFYDW3QHTIL7IPOWTBCSUEZK7.ttf')
+  ).then((res) => res.arrayBuffer());
+
   return new ImageResponse(
     (
       <div
@@ -16,20 +20,30 @@ export default async function Image() {
           display: 'flex',
           alignItems: 'center',
           justifyContent: 'center',
-          fontFamily: 'sans-serif',
-          fontSize: 140,
-          fontWeight: 800,
-          letterSpacing: '-0.04em',
+          fontFamily: '"Clash Display"',
+          fontSize: 180,
+          fontWeight: 700,
+          letterSpacing: '-0.02em',
           color: '#050505',
         }}
       >
-        <div style={{ display: 'flex', alignItems: 'center' }}>
-          KRI
+        <div style={{ display: 'flex', alignItems: 'center', gap: '16px' }}>
+          <span>KRI</span>
           <span style={{ color: '#d2f34d', fontStyle: 'italic' }}>4</span>
-          TIV
+          <span>TIV</span>
         </div>
       </div>
     ),
-    { ...size }
+    {
+      ...size,
+      fonts: [
+        {
+          name: 'Clash Display',
+          data: clashDisplayData,
+          style: 'normal',
+          weight: 700,
+        },
+      ],
+    }
   );
 }
